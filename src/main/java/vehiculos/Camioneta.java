5a70f632688003f1b7771e285a7447553939ec95
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-05-09 14:05:15 -0500

    .

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -10,7 +10,7 @@ public class Camioneta extends Vehiculo{
     public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
         super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
         this.volco = volco;
-        listaCamionetas.add(this);
+        Camioneta.listaCamionetas.add(this);
     }
 
     public boolean isVolco() {
