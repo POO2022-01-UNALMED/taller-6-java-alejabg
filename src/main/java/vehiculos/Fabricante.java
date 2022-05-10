3d6fc19c8f10454df23ea00152f644949415bd04
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-05-10 07:45:46 -0500

    .

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,50 +1,31 @@
 package vehiculos;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Fabricante{
     
     private String nombre;
     private Pais pais;
     private static ArrayList<Fabricante> listaFabricantes = new ArrayList<>();
+    private int vehiculos;
    
     public Fabricante(String nombre, Pais pais){
         this.nombre = nombre;
         this.pais = pais;
         Fabricante.listaFabricantes.add(this);
+        this.vehiculos = 0;
     }
 
-    public static Fabricante fabricaMayorVentas() {
-		ArrayList<String> nombres = new ArrayList<>();
-		for (Vehiculo vehiculo : Vehiculo.getListaVehiculos()) {
-			nombres.add(vehiculo.getFabricante().getNombre());
-		}
-		HashMap<String, Integer> dic = new HashMap<>();
-		for (int x = 0; x < nombres.size(); x++) {
-			String nombre = nombres.get(x);
-			if (dic.containsKey(nombre)) {
-				dic.put(nombre, dic.get(nombre) + 1);
-			}else {
-				dic.put(nombre, 1);
+    public static Fabricante fabricaMayorVentas(){
+		Fabricante ganador = new Fabricante(null, null);
+		
+		for(Fabricante fabricante: listaFabricantes) {
+			if(ganador.vehiculos < fabricante.vehiculos) {
+				ganador = fabricante;
 			}
 		}
 		
-		String x = "";
-		int mayor = 0;
-		for (HashMap.Entry<String, Integer> entry : dic.entrySet()) {
-			if (entry.getValue() > mayor) {
-				mayor = entry.getValue();
-				x = entry.getKey();
-			}
-		}
-
-		for (Fabricante fabricante : Fabricante.getListaFabricantes()) {
-			if (fabricante.getNombre() == x) {
-				return fabricante;
-			}
-		}
-        return null;
+		return ganador;
     }
 
     public String getNombre() {
@@ -54,7 +35,6 @@ public class Fabricante{
         this.nombre = nombre;
     }
 
-  
     public Pais getPais() {
         return pais;
     }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,16 +1,28 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Pais {
 
     private String nombre;
+    private static ArrayList<Pais> listaPaises = new ArrayList<>();
+    private int vehiculos;
 
     public Pais(String nombre) {
         this.nombre = nombre;
+        this.vehiculos = 0;
     }
     
-    public static Pais paisMasVendedor() {
-        return Fabricante.fabricaMayorVentas().getPais();
-    }
+    public static Pais paisMasVendedor(){
+		Pais ganador = new Pais(null);
+		
+		for(Pais pais: listaPaises) {
+			if(ganador.vehiculos < pais.vehiculos) {
+				ganador = pais;
+			}
+		}		
+		return ganador;
+	}
 
     public String getNombre() {
         return nombre;
